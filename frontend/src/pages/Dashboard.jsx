2885313b2059b4d830bace8e046a0e6ef4a8203a
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-11 15:41:38 +0530

    Create dashboard page

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,36 @@
+import Sidebar from "../components/Sidebar";
+import StatsCards from "../components/StatsCards";
+import DeploymentForm from "../components/DeploymentForm";
+import DeploymentTable from "../components/DeploymentTable";
+import DeploymentTimeline from "../components/DeploymentTimeline";
+import "../styles/dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard-container">
+      <Sidebar />
+
+      <div className="main-content">
+        <h2 className="mb-4">☁ CloudDeployX Dashboard</h2>
+
+        <StatsCards />
+
+        <div className="row mt-4">
+          <div className="col-lg-6">
+            <DeploymentForm />
+          </div>
+
+          <div className="col-lg-6">
+            <DeploymentTimeline />
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <DeploymentTable />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
