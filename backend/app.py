6f4c13c5fe9fc895a59c9ae95259773c900f1609
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-11 16:10:43 +0530

    Update Flask application

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,27 @@
+# from flask import Flask
+# from flask_cors import CORS
+# from routes.deployment_routes import deployment_bp
+
+# app = Flask(__name__)
+# CORS(app)
+
+# app.register_blueprint(deployment_bp)
+
+# @app.route("/")
+# def home():
+#     return {
+#         "message": "CloudDeployX Backend Running Successfully"
+#     }
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 from flask import Flask
 from flask_cors import CORS
 from routes.deployment_routes import deployment_bp
+from config import Config
+
+print("Mongo URI =", Config.MONGODB_URI)
 
 app = Flask(__name__)
 CORS(app)
