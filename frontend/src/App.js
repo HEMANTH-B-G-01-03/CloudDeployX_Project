e46bc1c65f4e015ef159e3afe1ff303ce9ef4cdf
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-11 16:40:30 +0530

    Configure React Router routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import Deployments from "./pages/Deployments";
+import DockerImages from "./pages/DockerImages";
+import Kubernetes from "./pages/Kubernetes";
+import Monitoring from "./pages/Monitoring";
+import Settings from "./pages/Settings";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/deployments" element={<Deployments />} />
+        <Route path="/docker-images" element={<DockerImages />} />
+        <Route path="/kubernetes" element={<Kubernetes />} />
+        <Route path="/monitoring" element={<Monitoring />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
