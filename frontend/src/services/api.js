07ac8f159bd423cbfb033b23d9d96d4008039cba
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-07-08 21:37:02 +0530

    Updated API configuration and improved dashboard styling

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://54.205.237.4:5000",
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://54.205.237.4:5000",
+  baseURL: "http://localhost:5000",
 });
 
 export default API;
